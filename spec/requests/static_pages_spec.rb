4a58f9a024897ca7a6f9d4be11e9d0e70c497f48
Justin White <jstnwht1991@gmail.com>
2014-03-27 16:14:53 -0700

    Add full titler helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,9 +8,13 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('OurLocal')
     end
-     it "should have the title 'Home'" do
+     it "should have the base title" do
       visit '/static_pages/home'
-      expect(page).to have_title("OurLocal | Home")
+      expect(page).to have_title("OurLocal")
+    end
+    it "should not have a custom page title" do
+      visit '/static_pages/home'
+      expect(page).not_to have_title('| Home')
     end
   end
    describe "Help page" do
@@ -33,6 +37,18 @@ describe "Static pages" do
      it "should have the title 'About'" do
       visit '/static_pages/about'
       expect(page).to have_title("OurLocal | About")
+    end
+    end
+    describe "Contact page" do
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("OurLocal | Contact")
     end
     end
 end
